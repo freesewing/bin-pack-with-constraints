b7c6a748ae596ff5cf3203ab1a22478e6f7c6e61
Benjamin F <example@example.com>
2023-05-19 19:35:16 -0700

    fix: Added strictMax support and improved strategy for layouts that have a maximum width

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ module.exports = function(items, options) {
 	// Clone the items.
 	var newItems = items.map(function(item) { return inPlace ? item : { width: item.width, height: item.height, item: item }; });
 
+	// We need to know whether the widest item exceeds the maximum width.
+	// The optimal sorting strategy changes depending on this.
+	const widestWidth =
+		newItems.sort(function(a, b) { return b.width - a.width})[0].width
+	const widerThanMax = options.maxWidth && (widestWidth > options.maxWidth)
+
 	newItems = newItems.sort(function(a, b) {
-		if (options.maxWidth && !options.maxHeight) return b.width - a.width
+		if (options.maxWidth && !options.maxHeight && widerThanMax) return b.width - a.width
+		if (options.maxWidth && !options.maxHeight) return b.height - a.height
 		if (options.maxHeight) return b.height - a.height
 		// TODO: check that each actually HAS a width and a height.
 		// Sort based on the size (area) of each block.
